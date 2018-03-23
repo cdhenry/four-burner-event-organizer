6576fdabfc3754d6808729e48a74abcadace3907
Chris Henry <cdhenry@gmail.com>
2018-03-23 15:38:07 +0000

    added more burner relationships

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,8 @@ class Event < ActiveRecord::Base
 
   belongs_to :creator, class_name: 'User'
   has_many :user_events
-  has_many :event_event_types
-  has_many :users, :through => :user_events
-  has_many :event_types, :through => :event_event_types
+  #has_many :event_event_types
+  has_many :users, through: :user_events
+  has_many :burners, through: :users
+  #has_many :event_types, :through => :event_event_types
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,9 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  has_many :burners
   has_many :events_created, foreign_key: :creator_id, class_name: "Event"
   has_many :user_events
   has_many :events, through: :user_events
-  has_many :event_types, through: :events
+  #has_many :event_types, through: :events
 end

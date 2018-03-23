daa3beafc8480da4d856f9929577c6e82d50332e
Chris Henry <cdhenry@gmail.com>
2018-03-23 18:18:43 +0000

    recreated to burners controller

--- app/controllers/burner_controller.rb
+++ app/controllers/burner_controller.rb
@@ -1,19 +1,11 @@
 class BurnersController < ApplicationController
-  get '/users/:id/burners' do
-    if params[:id] == current_user.id
-      erb :'users/burners/index'
-    else
-      redirect to '/login'
-    end
+  get '/burners' do
+    @burners = Burner.all
+    erb :'burners/index'
   end
-
-  # get '/burners' do
-  #   @burners = Burner.all
-  #   erb :'burners/index'
-  # end
-  #
-  # get '/burners/:slug' do
-  #   @burner = Burner.find_by_slug(params[:slug])
-  #   erb :'/burners/show'
-  # end
+  
+  get '/burners/:slug' do
+    @burner = Burner.find_by_slug(params[:slug])
+    erb :'/burners/show'
+  end
 end

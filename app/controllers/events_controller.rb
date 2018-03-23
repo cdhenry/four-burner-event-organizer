3c20960b4eac78ef6c8131a7ad76a71209aeb1a2
Chris Henry <cdhenry@gmail.com>
2018-03-23 18:13:27 +0000

    recreated events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   get '/events' do
     if logged_in?
-      @public_events = Event.all.select{|event| event.public = true}
+      @events = Event.all
       erb :'events/index'
     else
       redirect to '/login'
@@ -21,14 +21,9 @@ class EventsController < ApplicationController
       if params[:description] == "" || params[:name] == ""
         redirect to "/events/new"
       else
-        event = Event.new(name: params[:name], description: params[:description], public: params[:public])
-        # if !params["event_type"]["name"].empty? && !EventType.find_by(name: params["event_type"]["name"])
-        #   event.event_types << EventType.new(name: params["event_type"]["name"])
-        # end
-        if event.save
-          event.creator = current_user
-          current_user.events << event
-          redirect to "/users/#{current_user.id}"
+        @event = Event.new(name: params[:name], description: params[:description], date_and_time: params[:date_and_time])
+        if @event.save
+          redirect to "/events/#{@event.id}"
         else
           redirect to "/events/new"
         end
@@ -62,11 +57,11 @@ class EventsController < ApplicationController
 
   patch '/events/:id' do
     if logged_in?
-      if params[:name] == "" || params[:event_type] == "" || params[:description] == ""
+      if params[:name] == "" || params[:description] == "" || params[:date_and_time] == ""
         redirect to "/events/#{params[:id]}/edit"
       else
         @event = Event.find_by_id(params[:id])
-        if @event && @event.creator == current_user
+        if @event && @event.user == current_user
           if @event.update(name: params[:name], description: params[:description])
             redirect to "/events/#{@event.id}"
           else
@@ -76,22 +71,6 @@ class EventsController < ApplicationController
           redirect to '/events'
         end
       end
-    else
-      redirect to '/login'
-    end
-  end
-
-  post '/events/:id' do
-    if logged_in?
-      event = Event.find_by_id(params[:id])
-      if params[:attending] && !current_user.events.include?(event)
-        current_user.events << event
-      elsif !params[:attending]
-        if current_user.events.include?(event)
-          current_user.events.delete(event.id)
-        end
-      end
-      redirect to "/users/#{current_user.id}"
     else
       redirect to '/login'
     end

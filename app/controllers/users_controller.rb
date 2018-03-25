8937bd33b7441e90b1c7de0098aa59d06278b14d
Chris Henry <cdhenry@gmail.com>
2018-03-25 20:34:37 +0000

    added about/welcome page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     if !logged_in?
       erb :'users/signup'
     else
-      redirect to '/events'
+      redirect to '/'
     end
   end
 
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
       @user = User.new(name: params[:name], email: params[:email], password: params[:password])
       @user.save
       session[:user_id] = @user.id
-      redirect to '/events'
+      redirect to '/'
     end
   end
 

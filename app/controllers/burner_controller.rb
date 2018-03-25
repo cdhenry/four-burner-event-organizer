cac251464ffa84094170c7ddc572e590285d8a12
Chris Henry <cdhenry@gmail.com>
2018-03-25 20:12:04 +0000

    fixed issue with burners view showing other users' events

--- app/controllers/burner_controller.rb
+++ app/controllers/burner_controller.rb
@@ -1,14 +1,16 @@
 class BurnersController < ApplicationController
   get '/burners' do
     if logged_in?
-      @burners = Burner.all
-      #@duration_hash = {}
-      duration_array = users_events.collect do |burner, events|
-        "['#{burner}', #{events.collect {|event| event.duration}.sum}]"
-        #@duration_hash[burner] = events.collect {|event| event.duration}.sum
+      if current_user.burners.empty?
+        redirect to 'events'
+      else
+        @burners = current_user.burners
+        duration_array = users_events.collect do |burner, events|
+          "['#{burner}', #{events.collect {|event| event.duration}.sum}]"
+        end
+        @chart_breakdown = duration_array.join(",")
+        erb :'burners/index'
       end
-      @chart_breakdown = duration_array.join(",")
-      erb :'burners/index'
     else
       redirect to '/login'
     end
@@ -16,7 +18,9 @@ class BurnersController < ApplicationController
 
   get '/burners/:slug' do
     if logged_in?
-      @burner = current_user.burners.find_by_slug(params[:slug])
+      @events = current_user.events.collect do |event|
+        event if event.burners.find_by_slug(params[:slug])
+      end
       erb :'/burners/show'
     else
       redirect to '/login'
